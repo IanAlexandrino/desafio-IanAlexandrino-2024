47e30d6b2b63b4c871820a54a5cd405f5235fe25
Ian Rodrigues <ianrodrigues2001@hotmail.com>
2024-09-10 23:37:19 -0300

    feat: adding a custom exception for Animal

---
+++ src/erros/erro-validacao-animal.js
@@ -0,0 +1,9 @@
+class ErroValidacaoAnimal extends Error {
+    constructor(msg, campo){
+        super(msg);
+        this.campo = campo;
+        this.name = "ErroValidacaoAnimal";
+    }
+}
+
+export { ErroValidacaoAnimal };
